497af0ea7b02b3c534f95d961a3b9949d8bc2763
Jeff Puckett <jpuckett@dealerinspire.com>
2025-01-06 12:20:13 +0000

    add main.js

---
+++ main.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const file = process.argv[2];
+
+function splitOnLastCharacter(str, char) {
+  const lastIndex = str.lastIndexOf(char);
+
+  if (lastIndex === -1) {
+    return [str];
+  }
+
+  return [str.slice(0, lastIndex), str.slice(lastIndex + 1)];
+}
+
+function findUrlExtension(url) {
+    const noQuery = splitOnLastCharacter(url, "?")[0];
+    return splitOnLastCharacter(noQuery, ".")[1];
+}
+
+function pushNew(array, value) {
+  if (!array.includes(value)) {
+    array.push(value);
+  }
+}
+
+const extensions = [];
+
+fs.readFile(file, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  try {
+    const entries = JSON.parse(data).log.entries;
+
+    for (const key in entries) {
+      const url = entries[key].request.url;
+      if (url.startsWith("https://uat-autopreferred.chase.com")) {
+        console.log(url);
+        const extension = findUrlExtension(url)
+        pushNew(extensions, extension)
+        console.log(extension);
+        console.log(entries[key].request.queryString, "\n");
+      }
+    }
+  } catch (parseError) {
+    console.error("Error parsing JSON:", parseError);
+  }
+
+  console.log(extensions)
+});
